4ae98bc73fd5b7519ca4f98b922fb83530a6d44c
Sille Van Landschoot <info@sillevl.be>
2016-11-01 12:10:12 +0100

    add weather scaffold

---
+++ app/models/weather.rb
@@ -0,0 +1,8 @@
+class Weather
+  include NoBrainer::Document
+  include NoBrainer::Document::Timestamps
+
+  field :temperature, :type => Float
+  field :humidity, :type => Float
+  field :pressure, :type => Float
+end
